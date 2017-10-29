21cd88e4f1cb61ceef6f4cf676d753246cf84447
v4lt <victorbaverel@protonmail.com>
2017-10-30 00:57:14 +0100

    Version finale non commentee fifo

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=gcc
 CFLAGS=-Wall -Werror -g
 LDLIBS=-lpthread
 
-all: clean read_write_priority_writer read_write_priority_reader read_write_priority_fifo
+all: clean read_write_priority_writer read_write_priority_reader read_write_priority_fifo_v2
 
 
 read_write_priority_writer: read_write_priority_writer.c
@@ -11,8 +11,8 @@ read_write_priority_writer: read_write_priority_writer.c
 read_write_priority_reader: read_write_priority_reader.c
 	$(CC) $(CFLAGS) -o read_write_priority_reader read_write_priority_reader.c $(LDLIBS)
 
-read_write_priority_fifo: read_write_priority_fifo.c
-	$(CC) $(CFLAGS) -o read_write_priority_fifo read_write_priority_fifo.c $(LDLIBS)
+read_write_priority_fifo_v2: read_write_priority_fifo_v2.c
+	$(CC) $(CFLAGS) -o read_write_priority_fifo read_write_priority_fifo_v2.c $(LDLIBS)
 
 clean:
 	rm -f *.o
--- read_write_priority_fifo_v2.c
+++ read_write_priority_fifo_v2.c
@@ -1,169 +1,95 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <ctype.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <string.h>
-#include <time.h>
+#include "read_write_priority_fifo_v2.h"
 
 
-// DONNEES DU PROGRAMME
-
-// threads : Adresse vers les threads (les writers et lecteurs)
 pthread_t *threads;
 
-// Structure pour l'information des threads :
-// identifiant : un identifiant pour la position dans la liste fifo
-typedef struct struct_info_thread
-{
-  int identifiant;
-  pthread_cond_t cond;
-}struct_info_thread;
-
-// Structure modelisant le cas de first-in-first-out :
-// mutex_variable : un mutex pour un acces protege, utilise pour changer le nombre des readers actuel
-// cond_wakeup_writers : une condition pour pouvoir reveiller les writers en cas qu'ils peuvent ecrire
-// cond_wakeup_readers : une condition pour pouvoir reveiller les readers en cas qu'ils peuvent lire
-// identifiant_global : un identifiant pour la position dans la liste fifo
-// nb_reader : le nombre des lecteurs actuels
-// last_reader : le reader precedent dans la liste fifo
-// info_thread : une structure pour l'information de chaque thread
-typedef struct struct_priority_fifo{
-  pthread_mutex_t mutex_variable;
-  pthread_cond_t  cond_wakeup_writers;
-  pthread_cond_t  cond_wakeup_readers;
-
-  int identifiant_global;
-  int nb_reader;
-  int last_reader;
-  clock_t start;
-  struct_info_thread * info_thread;
-
-}struct_priority_fifo;
-
-// Une pointeur vers une structure fifo
+/*
+Variable globale à la totalité de nos threads, permettant d'accéder
+aux mutex etc (voir .h)
+*/
 struct_priority_fifo *struct_priority;
 
+//Tableau d'entier permettant de savoir le rôle de chaque ticket
+int *array_of_role_per_ticket;
 
-// FONCTIONS DU PROGRAMME
 
-// Intialisation de la structure pour fifo
-// Allocation un espace memoire pour la structure
-// Initialisation des conditions et du mutex
-// Initialisation du dernier reader a 100
-// Initialisation du nombre des readers actuels a 0
-// Initialisation de la position globale dans la liste a 0
-void init_priority_fifo()
+void init_priority_fifo(int nb_thread, int nb_iterations)
 {
   struct_priority = malloc(sizeof(struct_priority_fifo));
   pthread_cond_init(&(struct_priority->cond_wakeup_readers),NULL);
   pthread_cond_init(&(struct_priority->cond_wakeup_writers),NULL);
   pthread_mutex_init(&(struct_priority->mutex_variable),NULL);
-  struct_priority->last_reader=100;
-  struct_priority->nb_reader = 0;
+  pthread_mutex_init(&(struct_priority->mutex_distrib),NULL);
+
   struct_priority->identifiant_global=0;
-  struct_priority->start = clock();
+  struct_priority->distrib_ticket=0;
+
+  struct_priority->nb_reader = 0;
+  struct_priority->nb_thread = nb_thread;
+  struct_priority->nb_iterations = nb_iterations;
 }
 
-// Fonction random
+void destruct_priority_fifo()
+{
+  free(threads);
+  free(array_of_role_per_ticket);
+  pthread_mutex_destroy(&(struct_priority->mutex_variable));
+  pthread_mutex_destroy(&(struct_priority->mutex_distrib));
+  pthread_cond_destroy(&(struct_priority->cond_wakeup_readers));
+  pthread_cond_destroy(&(struct_priority->cond_wakeup_writers));
+  free(struct_priority);
+}
+
+// FONCTIONS UTILITAIRES //
+
+//Fonction renvoyant 1 ou 0
 int hasard(int inf, int sup) {
    return inf +
    (int)((double)(sup - inf + 1) * rand() / ((double)RAND_MAX + 1));
 }
 
-//Fonction permettant de savoir si un thread écrivain peut lire si une lecture est déjà
-//En cours afin de permettre un accès parallèle au fichier
-/*int keep_reading(int id_global, int idx)
+//Sleep. Source: test_lecteurs_redacteurs.c
+void dodo(int scale) {
+    usleep((random()%1000000)*scale);
+}
+
+/*
+Fonction permettant de savoir si un thread ayant le ticket idx se trouve
+Dans le bloc de lecteur courant, si c'est le cas, on renvoi 1, sinon, 0
+*/
+int in_reader_block(int id_ticket)
 {
-  printf("%s%d%s%d","Keep Reading ",idx, "avec i =",id_global );
-  if(id_global==idx)
-    return 0;
-  while(id_global!=idx)
-  {
-    if(array_of_role_per_thread[id_global])
+  int id_global = struct_priority->identifiant_global;
+  if((id_global+1)==(struct_priority->nb_thread*struct_priority->nb_iterations))
+    if(array_of_role_per_ticket[id_global+1])
     {
-        printf("%s%d\n"," Retour : ",0);
-        return 0;
+        return 1;
     }
+  while(id_global!=id_ticket)
+  {
+    if(!array_of_role_per_ticket[id_global])
+        return 0;
     id_global++;
   }
-  printf("%s%d\n"," Retour : ",1);
   return 1;
 }
-DEPRECATED
-*/
 
-// Bloquage du writer pour attendre son tour ou que le nombre des lecteurs actuels est egal a 0 :
-// Verification qu'une des deux conditions soit correcte
-// Dans ce cas on reveille tous les readers
-// Et on attend d'etre reveille par un reader pour reverifier les conditions
-void writer_lock(struct_info_thread *struct_personal)
+void func_distrib_tickets(int type, struct_info_thread* struct_personal)
 {
-
-
+  pthread_mutex_lock(&(struct_priority->mutex_distrib));
+  int ticket = struct_priority->distrib_ticket;
+  struct_priority->distrib_ticket++;
+  struct_personal->id_ticket=ticket;
+  array_of_role_per_ticket[ticket]=type;
+  if(!type)
+    printf("%d%s%d%s\n", struct_personal->identifiant,KRED"?"KNRM,ticket," ");
+  else
+    printf("%d%s%d%s\n", struct_personal->identifiant,KGRN"?"KNRM,ticket," ");
+  pthread_mutex_unlock(&(struct_priority->mutex_distrib));
 }
 
-// Ecriture du message pour un writer
-void write_msg(struct_info_thread *struct_personal)
-{
-  struct_personal->diff= clock() - struct_priority->start;
-  int msec = struct_personal->diff * 1000 / CLOCKS_PER_SEC;
-  printf("%s%d%s%f%s%f%s\n", "Je suis ecrivain mon identifiant est: ",struct_personal->identifiant, " Temps: ",msec/1000.0, " ms et ", msec/1000000.0, " ns.");
-  sleep(0.3);
-}
+//FONCTIONS EN LIEN AVEC ECRIVAIN//
 
-// Avancement de la liste pour un writer :
-// Incrementation de la position dans la liste
-// On reveille tous les readers
-void writer_unlock(struct_info_thread *struct_personal)
-{
-
-
-}
-
-// Bloquage du reader pour attendre son tour :
-// Verification que la condition soit vraie et dans ce cas
-// On attend d'etre reveille par un writer et on re-teste
-// En cas contraire, on incremente le nombre des readers et on incremente la position
-void reader_lock(struct_info_thread *struct_personal)
-{
-
-
-}
-
-// Ecriture du message par un reader
-void read_msg(struct_info_thread *struct_personal)
-{
-  struct_personal->diff= clock() - struct_priority->start;
-  int msec = struct_personal->diff * 1000 / CLOCKS_PER_SEC;
-  printf("%s%d%s%f%s%f%s\n", "Je suis lecteur mon identifiant est: ",struct_personal->identifiant, " Temps: ",msec/1000.0, " ms et ", msec/1000000.0, " ns.");
-  sleep(0.3);
-}
-
-// Avancement de la liste pour un reader :
-// Changement du dernier lecteur pour dire qu'on etait le dernier
-// Decrementation du nombre des lecteurs actuels
-void reader_unlock(struct_info_thread *struct_personal)
-{
-
-}
-
-// Le fonctionnement d'un thread reader :
-// Bloquage d'un reader pour attendre son tour
-// Ecriture du message quand c'est son tour
-// Avancement de la liste d'attente
-void* thread_reader(void* data)
-{
-  struct_info_thread* struct_personal;
-  struct_personal = (struct_info_thread*) data;
-  printf("%s%d\n", "[INIT]Je suis lecteur mon identifiant est: ",(struct_personal->identifiant));
-  reader_lock(struct_personal);
-  read_msg(struct_personal);
-  reader_unlock(struct_personal);
-
-  return NULL;
-}
 
 // Le fonctionnement d'un thread writer :
 // Bloquage d'un writer pour attendre son tour
@@ -173,63 +99,179 @@ void* thread_writer(void* data)
 {
   struct_info_thread* struct_personal;
   struct_personal = (struct_info_thread*) data;
-  printf("%s%d\n", "[INIT]Je suis ecrivain mon identifiant est: ",(struct_personal->identifiant));
-  writer_lock(struct_personal);
-  write_msg(struct_personal);
-  writer_unlock(struct_personal);
+  for (int i = 0; i < struct_priority->nb_iterations; i++)
+  {
+    func_distrib_tickets(0,struct_personal);
+    writer_lock(struct_personal);
+    write_msg(struct_personal);
+    writer_unlock(struct_personal);
+  }
+  free(data);
+  return NULL;
+}
+
+
+void writer_lock(struct_info_thread *struct_personal)
+{
+  pthread_mutex_lock(&(struct_priority->mutex_variable));
+  int id = struct_personal->id_ticket;
+  while(id!=struct_priority->identifiant_global||struct_priority->nb_reader>0)
+  {
+    pthread_cond_broadcast(&(struct_priority->cond_wakeup_readers));
+    pthread_cond_wait(&(struct_priority->cond_wakeup_readers),
+    &(struct_priority->mutex_variable));
+  }
+
+  pthread_mutex_unlock(&(struct_priority->mutex_variable));
+}
+
+// Ecriture du message pour un writer
+void write_msg(struct_info_thread *struct_personal)
+{
+  printf("%s%d%s%d%s\n", KRED,struct_personal->identifiant, "-", struct_personal->id_ticket,KNRM " ");
+  dodo(2);
+}
+
+// Avancement de la liste pour un writer :
+// Incrementation de la position dans la liste
+// On reveille tous les readers
+void writer_unlock(struct_info_thread *struct_personal)
+{
+  pthread_mutex_lock(&(struct_priority->mutex_variable));
+  struct_priority->identifiant_global++;
+  if(struct_priority->identifiant_global+1==(struct_priority->nb_thread*struct_priority->nb_iterations))
+    pthread_cond_signal(&(struct_priority->cond_wakeup_readers));
+  pthread_mutex_unlock(&(struct_priority->mutex_variable));
+}
+
+//FIN DES FONCTIONS EN LIEN AVEC ECRIVAIN//
+
+
+
+//FONCTIONS EN LIEN AVEC LECTEURS//
+
+// Bloquage du reader pour attendre son tour :
+// Verification que la condition soit vraie et dans ce cas
+// On attend d'etre reveille par un writer et on re-teste
+// En cas contraire, on incremente le nombre des readers et on incremente la position
+
+// Le fonctionnement d'un thread reader :
+// Bloquage d'un reader pour attendre son tour
+// Ecriture du message quand c'est son tour
+// Avancement de la liste d'attente
+void* thread_reader(void* data)
+{
+  struct_info_thread* struct_personal;
+  struct_personal = (struct_info_thread*) data;
+  for (int i = 0; i < struct_priority->nb_iterations; i++)
+  {
+    func_distrib_tickets(1,struct_personal);
+    reader_lock(struct_personal);
+    read_msg(struct_personal);
+    reader_unlock(struct_personal);
+  }
+  free(data);
 
   return NULL;
 }
 
 
-// PROGRAMME PRINCIPALE
+void reader_lock(struct_info_thread *struct_personal)
+{
+  pthread_mutex_lock(&(struct_priority->mutex_variable));
+  int id = struct_personal->id_ticket;
+  while(struct_priority->identifiant_global!=id||!in_reader_block(id))
+  {
+      pthread_cond_broadcast(&(struct_priority->cond_wakeup_readers));
+      pthread_cond_wait(&(struct_priority->cond_wakeup_readers),
+      &(struct_priority->mutex_variable));
+  }
+
+  struct_priority->nb_reader ++;
+  struct_priority->identifiant_global++;
+  pthread_mutex_unlock(&(struct_priority->mutex_variable));
+}
+
+// Ecriture du message par un reader
+void read_msg(struct_info_thread *struct_personal)
+{
+    printf("%s%d%s%d%s\n", KGRN,struct_personal->identifiant, "*", struct_personal->id_ticket,KNRM " ");
+    dodo(2);
+}
+
+// Avancement de la liste pour un reader :
+// Changement du dernier lecteur pour dire qu'on etait le dernier
+// Decrementation du nombre des lecteurs actuels
+void reader_unlock(struct_info_thread *struct_personal)
+{
+  pthread_mutex_lock(&(struct_priority->mutex_variable));
+  struct_priority->nb_reader --;
+  if(struct_priority->identifiant_global+1==(struct_priority->nb_thread*struct_priority->nb_iterations))
+    pthread_cond_signal(&(struct_priority->cond_wakeup_readers));
+  pthread_mutex_unlock(&(struct_priority->mutex_variable));
+}
+
+//FIN DES FONCTIONS EN LIEN AVEC LECTEURS//
+
+
+// PROGRAMME PRINCIPAL
 
 int main(int argc, char **argv)
 {
 
-  srand(time(NULL)) ; // Intialisation de la seed pour les randoms
-  int nb_thread = 0;
-  int idx_thread= 0;
+  // Intialisation de la seed pour les randoms
+  srand(time(NULL)) ;
+
+  int nb_thread,idx_thread,nb_lecteurs,nb_redacteurs,nb_iterations;
 
   // Verification du nombre d'arguments
-  if(argc<1)
-  {
-    printf("%s","Merci d'entrer le nombre de thread" );
-    return 0;
+  if (argc < 4) {
+      fprintf(stderr, "Utilisation: %s nb_lecteurs nb_redacteurs "
+                      "nb_iterations\n", argv[0]);
+      exit(1);
   }
 
-  // Initialisation
-  init_priority_fifo();
+  printf("%s\n","Symbole : " );
+  printf(KRED "-" KNRM );
+  printf("%s\n","  : Ecriture" );
+  printf(KGRN "*" KNRM );
+  printf("%s\n","  : Lecture" );
+  printf("%s\n\n","?  : Prise de ticket" );
+  printf("%s\n","Notations: " );
+  printf(KRED "Ecriture -> [Identifiant Thread Ecrivain]-[Identifiant Ticket]\n");
+  printf(KGRN "Lecture  -> [Identifiant Thread Lecteur]*[Identifiant Ticket]\n");
+  printf(KNRM"Prise Ticket Lecteur  -> [Identifiant Thread Lecteur]"KGRN"?"KNRM"[Identifiant Ticket]\n");
+  printf("Prise Ticket Ecrivain -> [Identifiant Thread Ecrivain]"KRED"?"KNRM"[Identifiant Ticket]\n\n");
 
-  nb_thread=(int)strtol(argv[1], (char  **)NULL, 10);
-  printf("%d\n",nb_thread );
+  nb_lecteurs=   atoi(argv[1]);
+  nb_redacteurs= atoi(argv[2]);
+  nb_iterations= atoi(argv[3]);
+  nb_thread = nb_lecteurs + nb_redacteurs;
+
+  init_priority_fifo(nb_thread,nb_iterations);
+
   threads = malloc(nb_thread*sizeof(pthread_t));
+  array_of_role_per_ticket = malloc((nb_thread*nb_iterations)*sizeof(int));
 
   // Creation des threads
-  while(idx_thread!=nb_thread)
-  {
+  for (idx_thread = 0; idx_thread < nb_lecteurs; idx_thread++) {
     struct_info_thread *info_thread=malloc(sizeof(struct_info_thread));
-    if(hasard(0,1))
-    {
-      info_thread->identifiant=idx_thread;
-      pthread_create(&threads[idx_thread],NULL,thread_reader,info_thread);
-    }
-    else
-    {
-      info_thread->identifiant=idx_thread;
-      pthread_create(&threads[idx_thread],NULL,thread_writer,info_thread);
-    }
-    idx_thread++;
+    info_thread->identifiant=idx_thread;
+    pthread_create(&threads[idx_thread],NULL,thread_reader,info_thread);
+  }
+  for (int idx = idx_thread; idx < (nb_redacteurs+idx_thread); idx++) {
+    struct_info_thread *info_thread=malloc(sizeof(struct_info_thread));
+    info_thread->identifiant=idx;
+    pthread_create(&threads[idx],NULL,thread_writer,info_thread);
   }
 
-  idx_thread = 0;
 
-  // Fin de lecture
-  while(idx_thread!=nb_thread)
-  {
-      pthread_join(threads[idx_thread],NULL);
-      idx_thread++;
-  }
+  //array_of_role_per_ticket[idx_thread]=0;
+
+  for (idx_thread = 0; idx_thread < nb_thread; idx_thread++)
+    pthread_join(threads[idx_thread],NULL);
+
+  destruct_priority_fifo();
 
   return 0;
 }
